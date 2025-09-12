e676ca70e36443c670a37962c44de6f40de27090
Vishnu <vishnusankarip18@gmail.com>
2025-09-12 23:39:02 +0530

    axios change

--- src/config/axiosinstance.js
+++ src/config/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'http://13.60.46.96:8000/',
  
   headers: {
     'Accept': 'application/json',
